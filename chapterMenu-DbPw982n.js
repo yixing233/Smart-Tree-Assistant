451b6c311e2280bb3664757c2b5773ec7b1f5355
yixing233 <2191453326@qq.com>
2026-04-19 01:29:15 +0800

    测试内容移除

--- chapterMenu-DbPw982n.js
+++
@@ -1,2 +0,0 @@
-import{aA as t}from"./index-CQ8EGrtD.js";const s=t("chapterStore",{state:()=>({itemNum:null,currentItemId:null,isVisible:!1,activeKnowledgeId:null}),actions:{updateItemNum(e){this.itemNum+=e},updateCurrentItemId(e){this.currentItemId=e},updateIsVisible(e){this.isVisible=e},updateActiveKnowledgeId(e){this.activeKnowledgeId=e}}});export{s as u};
-
--- knowledgeStudy-wDaDtHg_.js
+++
@@ -1,2 +0,0 @@
-import{aA as r}from"./index-CQ8EGrtD.js";const e=r("curInfo",{state:()=>({curInfo:{}}),actions:{saveCurInfo(o){this.curInfo=o}}});export{e as u};
-
